7b61b2f99f72ce4d400812dad50a11e3c686e8d1
Vladimir16161616 <40914216+Vladimir16161616@users.noreply.github.com>
2019-03-24 16:08:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
+            int c = (a * 100)*2;
+            int d = (b * 2);
+            int e = (c + d);
+            int resultMod = (e % 1100);
+            Console.WriteLine((e/100) + "" + resultMod);
+        }
+    }
+}
